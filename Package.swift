462e9c74b0608edcf754442eb9658e2ca3e823ab
Madalina Ardelean <madalina.ardelean@kingfisher.com>
2025-08-20 11:53:22 +0100

    Update Braze to 13.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tealium/tealium-swift", .upToNextMajor(from: "2.16.0")),
-        .package(url: "https://github.com/braze-inc/braze-swift-sdk", .upToNextMajor(from: "11.0.0"))
+        .package(url: "https://github.com/braze-inc/braze-swift-sdk", .upToNextMajor(from: "13.0.0"))
     ],
     targets: [
         .target(
